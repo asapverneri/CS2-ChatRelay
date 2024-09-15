80bcbfcbb27cefd8b4ad9c5082df8a197875eb43
asapverneri <mattikaikkola@gmail.com>
2024-09-16 02:47:37 +0300

    v1.0
    Initial Commit

---
+++ ChatRelay.cs
@@ -0,0 +1,109 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ChatRelay;
+
+public class ChatRelayConfig : BasePluginConfig
+{
+    [JsonPropertyName("IgnoreCommands")] public bool IgnoreCommands { get; set; } = true;
+
+    [JsonPropertyName("DiscordWebhook")] public string DiscordWebhook { get; set; } = "";
+
+    [JsonPropertyName("EmbedFooter")] public string EmbedFooter { get; set; } = "ChatRelay (1.0) by verneri";
+
+}
+public class ChatRelay : BasePlugin, IPluginConfig<ChatRelayConfig>
+{
+    public override string ModuleName => "ChatRelay";
+    public override string ModuleDescription => "send your cs2 server chat messages to discord";
+    public override string ModuleAuthor => "verneri";
+    public override string ModuleVersion => "1.0";
+
+    public ChatRelayConfig Config { get; set; } = new();
+
+    public void OnConfigParsed(ChatRelayConfig config)
+    {
+        Config = config;
+    }
+    public override void Load(bool hotReload)
+    {
+        Logger.LogInformation($"loaded successfully! (Version {ModuleVersion})");
+        RegisterEventHandler<EventPlayerChat>(OnEventPlayerChat);
+    }
+
+    public HookResult OnEventPlayerChat(EventPlayerChat @event, GameEventInfo info)
+    {
+        var eventplayer = @event.Userid;
+        var player = Utilities.GetPlayerFromUserid(eventplayer);
+        if (player == null || !player.IsValid ||  @event.Text == null)
+            return HookResult.Continue;
+
+        if (Config.IgnoreCommands)
+        {
+            if (@event.Text.Contains('!') || @event.Text.Contains('/'))
+                return HookResult.Continue;
+        }
+
+        string playerteam = "[ALL]";
+        if (@event.Teamonly)
+        {
+            switch ((CsTeam)player.TeamNum)
+            {
+                case CsTeam.Terrorist:
+                    playerteam = "[T]";
+                    break;
+                case CsTeam.CounterTerrorist:
+                    playerteam = "[CT]";
+                    break;
+                case CsTeam.Spectator:
+                    playerteam = "[SPEC]";
+                    break;
+                case CsTeam.None:
+                    playerteam = "[NONE]";
+                    break;
+            }
+        }
+
+
+        _ = SendWebhookMessageAsEmbed(player.PlayerName, @event.Text, playerteam, player.SteamID);
+        return HookResult.Continue;
+    }
+
+    public async Task SendWebhookMessageAsEmbed(string playerName, string msg, string playerteam, ulong steamID)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var embed = new
+            {
+                title = playerName,
+                url = $"https://steamcommunity.com/profiles/{steamID}",
+                description = $"{playerteam}: {msg}",
+                color = 16760667,
+                footer = new
+                {
+                    text = $"{Config.EmbedFooter}"
+                }
+            };
+
+            var payload = new
+            {
+                embeds = new[] { embed }
+            };
+
+            var jsonPayload = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+            var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync(Config.DiscordWebhook, content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Logger.LogInformation($"Error while sending message to Discord! code: {response.StatusCode}");
+            }
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+## 🎮 CS2 ChatRelay
+
+Ligtweight plugin to send your cs2 server chat messages to discord channel using webhook.
+Tested on Windows.
+
+![GitHub tag (with filter)](https://img.shields.io/github/v/tag/asapverneri/CS2-ChatRelay?style=for-the-badge&label=Version)
+![Last Commit](https://img.shields.io/github/last-commit/asapverneri/CS2-ChatRelay?style=for-the-badge)
+
+---
+
+## 📦 Installion
+
+- Install [CounterStrike Sharp](https://github.com/roflmuffin/CounterStrikeSharp) & [Metamod:Source](https://www.sourcemm.net/downloads.php/?branch=master)
+- Download the latest release from the releases tab and copy it into the counterstrikesharp plugins folder.
+
+**Example config:**
+```json
+{
+  "IgnoreCommands": true,
+  "DiscordWebhook": "https://discord.com/api/webhooks/xxxxxx/xxxxxx",
+  "EmbedFooter": "ChatRelay (1.0) by verneri",
+  "ConfigVersion": 1
+}
+```
+
+---
+
+## 📫 Contact
+
+<div align="center">
+  <a href="https://discordapp.com/users/367644530121637888">
+    <img src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white" alt="Discord" />
+  </a>
+  <a href="https://steamcommunity.com/id/vvernerii/">
+    <img src="https://img.shields.io/badge/Steam-000000?style=for-the-badge&logo=steam&logoColor=white" alt="Steam" />
+  </a>
+</div>
+
+---
